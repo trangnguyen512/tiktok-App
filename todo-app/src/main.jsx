a4dfb4fd223a1d6c8a8a470f7e903ad5f3665f6d
Nguyen Trang <thuytranghthl@gmail.com>
2023-07-17 10:16:52 +0700

    setup

---
+++ todo-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Todo } from './components/Todo'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>,
+)
